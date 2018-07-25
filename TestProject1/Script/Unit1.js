02186be73accb203a9edc9ed8b110bc38bfc4160
JuliaBern <sun.daisy.fairy@gmail.com>
2018-07-25 22:18:28 +0300

    Initial commit

---
+++ TestProject1/Script/Unit1.js
@@ -0,0 +1,5 @@
+﻿function Test1()
+{
+  Browsers.Item(btFirefox).Run("https://smartbear.com/");
+  aqObject.CheckProperty(Aliases.browser.pageSoftwareTestingMonitoringDev.form.textnode, "contentText", cmpEqual, "Unleash your passion to create great software,\nthink bigger, and build smarter");
+}
